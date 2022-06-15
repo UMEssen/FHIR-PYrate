f55ebed4db03eeae03647916d01a93311af03a4f
giuliabaldini <giulia.baldini.11@gmail.com>
2022-06-15 08:52:49 +0200

    Add all to optional dependencies options

--- fhir_pyrate/__init__.py
+++ fhir_pyrate/__init__.py
@@ -4,13 +4,17 @@ import logging
 from fhir_pyrate.ahoy import Ahoy
 from fhir_pyrate.pirate import Pirate
 
+logging.basicConfig()
+
 DICOM_PACKAGES = ["dicomweb_client", "pydicom", "SimpleITK"]
 
 if importlib.util.find_spec("spacy") is None:
     logging.warning(
-        "SpaCy is not installed, so you will not be able to use the Miner."
-        "Please install spacy with pip install fhir-pyrate[miner] or add the "
-        "optional dependency `miner` to your package manager."
+        "The package spacy is not installed, so you will not be able to use the "
+        "Miner. "
+        "Please install them with pip install fhir-pyrate[miner] "
+        "(or fhir-pyrate[all]) or add the optional dependency `miner` "
+        "(or `all`) to your package manager."
     )
 else:
     from fhir_pyrate.miner import Miner
@@ -22,9 +26,10 @@ if any(not_installed):
     to_install = [package for b, package in zip(not_installed, DICOM_PACKAGES) if b]
     logging.warning(
         f"The packages {to_install} are not installed, so you will not be able to use the "
-        "DICOMDownloader."
-        "Please install them with pip install fhir-pyrate[downloader] or add the "
-        "optional dependency `downloader` to your package manager."
+        "DicomDownloader. "
+        "Please install them with pip install fhir-pyrate[downloader] "
+        "(or fhir-pyrate[all]) or add the optional dependency `downloader` "
+        "(or `all`) to your package manager."
     )
 else:
     from fhir_pyrate.dicom_downloader import DicomDownloader
