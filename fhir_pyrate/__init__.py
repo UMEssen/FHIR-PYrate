f4d79c6bea34aec49bac5a50f652804e5bfd8760
giuliabaldini <44327645+giuliabaldini@users.noreply.github.com>
2022-06-24 11:28:49 +0200

    Better import management with errors only on use (#55)
    * Better import management with errors only on use
    
    * Update imports with licence
    
    * Remove complete licence
    
    * Add changes to optional_import docstring

--- fhir_pyrate/__init__.py
+++ fhir_pyrate/__init__.py
@@ -1,39 +1,18 @@
-import importlib.util
 import logging
 
+import fhir_pyrate.util.imports
 from fhir_pyrate.ahoy import Ahoy
 from fhir_pyrate.pirate import Pirate
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
-DICOM_PACKAGES = ["dicomweb_client", "pydicom", "SimpleITK"]
-
-if importlib.util.find_spec("spacy") is None:
-    logger.warning(
-        "The package spacy is not installed, so you will not be able to use the "
-        "Miner. "
-        "Please install them with pip install 'fhir-pyrate[miner]' "
-        "(or 'fhir-pyrate[all]') or add the optional dependency `miner` "
-        "(or `all`) to your package manager."
-    )
-else:
-    from fhir_pyrate.miner import Miner
-
-not_installed = [
-    importlib.util.find_spec(package) is None for package in DICOM_PACKAGES
-]
-if any(not_installed):
-    to_install = [package for b, package in zip(not_installed, DICOM_PACKAGES) if b]
-    logger.warning(
-        f"The packages {to_install} are not installed, so you will not be able to use the "
-        "DicomDownloader. "
-        "Please install them with pip install 'fhir-pyrate[downloader]' "
-        "(or 'fhir-pyrate[all]') or add the optional dependency `downloader` "
-        "(or `all`) to your package manager."
-    )
-else:
-    from fhir_pyrate.dicom_downloader import DicomDownloader
+Miner, _ = fhir_pyrate.util.imports.optional_import(
+    module="fhir_pyrate.miner", name="Miner"
+)
+DicomDownloader, _ = fhir_pyrate.util.imports.optional_import(
+    module="fhir_pyrate.dicom_downloader", name="DicomDownloader"
+)
 
 __all__ = [
     "Ahoy",
